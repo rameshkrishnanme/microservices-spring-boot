a786acea7e27fc11b0dd6611ba3642ea248a29d7
Ramesh <ramesh.krishnan.uk@gmail.com>
2019-05-01 19:13:25 +0530

    Added Zipin, Rabbit MQ intergration

--- app-limit-service/src/main/java/com/mindtree/app/springboot/applimitservice/LimitsConfigurationController.java
+++ app-limit-service/src/main/java/com/mindtree/app/springboot/applimitservice/LimitsConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitsConfigurationController {
 
@@ -11,7 +13,12 @@ public class LimitsConfigurationController {
 	Configuration configuration;
 	
 	@GetMapping("/limits")
+	@HystrixCommand(fallbackMethod = "defaultMethod")
 	public LimitsConfiguration retrieveLimitsConfiguration() {
 		return new LimitsConfiguration(configuration.getMinimum(), configuration.getMaximum());
+	}
+	
+	public LimitsConfiguration defaultMethod() {
+		return new LimitsConfiguration(2, 33);
 	}
 }
--- app-zuul-api-server/src/main/java/com/mindtree/app/springboot/appzuulapiserver/ZuulLoggingFilter.java
+++ app-zuul-api-server/src/main/java/com/mindtree/app/springboot/appzuulapiserver/ZuulLoggingFilter.java
@@ -4,11 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
+@Component
 public class ZuulLoggingFilter extends ZuulFilter {
 	
 	Logger logger =	LoggerFactory.getLogger(this.getClass());
